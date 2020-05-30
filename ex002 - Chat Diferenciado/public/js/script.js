448999ad90e57ce482243f2de4bb96214d116c5c
Jonatan Frederico <jonatanfrederico@gmail.com>
2020-05-30 18:56:48 -0300

    Criação do segundo exemplo

--- ex002 - Chat Diferenciado/public/js/script.js
+++ ex002 - Chat Diferenciado/public/js/script.js
@@ -1,1 +1,62 @@
 var socket = io('http://localhost:3000');
+
+function loadUsuario(usr) {
+    console.log(socket.id);
+    document.getElementById("listaUsuarios").innerHTML += `<div id='${usr.id}' class='mensagem ${usr.id==socket.id?"autor":""}'>${usr.nome}</div>`;
+}
+function loadUsuarios(users) {
+    document.getElementById("listaUsuarios").innerHTML = "";
+    for (user of users) {
+        loadUsuario(user);
+    }
+}
+function addMensagem(data, eu) {
+    document.getElementById("mensagens").innerHTML += `<div class='mensagem ${eu ? "autor" : ""}'><h5>${data.hora}</h5><div class='msg'><h3>${data.autor}</h3> ${data.mensagem}</div>`;
+    updateScroll("mensagens");
+}
+function updateScroll(div){
+    var element = document.getElementById(div);
+    element.scrollTop = element.scrollHeight;
+}
+
+socket.on("usuarioEntrou", usr => {
+    console.log(usr);
+    loadUsuarios(usr);
+});
+
+socket.on("loadUsuarios", usuarios => {
+    for (usuario of usuarios){
+        loadUsuario(usuario);
+    }
+});
+
+socket.on("updateUsuarios", usuarios => {
+    loadUsuarios(usuarios);
+});
+
+socket.on("usuarioSaiu", usuario => {
+    document.getElementById(usuario).outerHTML = "";
+});
+
+socket.on("mensagemRecebida", (msg, eu) => {
+    addMensagem(msg, eu);
+});
+
+socket.on("mensagensAnteriores", mensagens => {
+    for (msg of mensagens) {
+        console.log(msg);
+        addMensagem(msg, false);
+    }
+});
+
+document.getElementById("mensagemContainer").onsubmit = event => {
+    event.preventDefault();
+
+    var autor = document.getElementById("autor");
+    var msg = document.getElementById("mensagem");
+
+    if (autor.value != "") {
+        socket.emit("updateUsuario", autor.value);
+    }
+    socket.emit("enviaMensagem", {id: socket.id, autor: autor.value, mensagem: msg.value, hora: new Date()});
+};
--- ex002 - Chat Diferenciado/server.js
+++ ex002 - Chat Diferenciado/server.js
@@ -5,7 +5,17 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+let usuarios = [];
 let mensagens = [];
+
+function pesquisaId(obj, id){
+    for (let x = 0; x < obj.length; x++){
+        if (obj[x].id == id){
+            return x;
+        }
+    }
+    return -1;
+}
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'public'));
@@ -18,6 +28,30 @@ app.use('/', (req, res) => {
 
 io.on('connection', socket => {
     console.log(`Socket conectado: ${socket.id}`);
+    usuarios.push({id: socket.id, nome: "Guest"});
+    
+    socket.emit("loadUsuarios", usuarios);
+    socket.broadcast.emit("usuarioEntrou", usuarios);
+
+    socket.emit("mensagensAnteriores", mensagens);
+
+    socket.on('disconnect', () => {
+        console.log(`Socket desconectado: ${socket.id}`);      
+        socket.broadcast.emit("usuarioSaiu", socket.id);
+        usuarios.splice(pesquisaId(usuarios, socket.id));
+    });
+
+    socket.on('updateUsuario', autor => {
+        usuarios[pesquisaId(usuarios, socket.id)].nome = autor;
+        socket.emit("updateUsuarios", usuarios);
+        socket.broadcast.emit("updateUsuarios", usuarios);
+    });
+
+    socket.on("enviaMensagem", msg => {
+        mensagens.push(msg);
+        socket.emit("mensagemRecebida", msg, true);
+        socket.broadcast.emit("mensagemRecebida", msg, false);
+    });
 });
 
 server.listen(3000);
